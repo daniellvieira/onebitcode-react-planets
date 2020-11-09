b5bf90486d24ae370ff4b80e37602024600b2ff9
Daniel Vieira <danielvieiraufc@gmail.com>
2020-11-09 11:44:18 -0300

    Controlando múltiplos inputs

--- src/components/planets/form/index.js
+++ src/components/planets/form/index.js
@@ -1,20 +1,47 @@
 import React, { Fragment, useState } from 'react'
 
 const Form = (props) => {
-  const [name, setName] = useState('')
-  const handleChange = (event) => setName(event.target.value)
+  const initialState = {
+    id: '',
+    name: '',
+    description: '',
+    img_url: '',
+    link: ''
+  }
+  const [fields, setFields] = useState(initialState)
+  const handleFieldsChange = (event) => setFields({
+    ...fields,
+    [event.currentTarget.name]: event.currentTarget.value
+  })
 
   const handleSubmit = event => {
-    props.addPlanet({name: name})
+    props.addPlanet(fields)
     event.preventDefault()
+    setFields(initialState)
   }
 
   return (
     <Fragment>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>     
+        {/* <div>
+          <label htmlFor="id">ID: </label>
+          <input type="text" name="id" id="id" value={fields.id} onChange={handleFieldsChange}/>
+        </div> */}
         <div>
           <label htmlFor="name">Name: </label>
-          <input type="text" id="name" value={name} onChange={handleChange}/>
+          <input type="text" name="name" id="name" value={fields.name} onChange={handleFieldsChange}/>
+        </div>
+        <div>
+          <label htmlFor="name">Description: </label>
+          <input type="text" name="description" id="description" value={fields.description} onChange={handleFieldsChange}/>
+        </div>
+        <div>
+          <label htmlFor="img_url">Image: </label>
+          <input type="text" name="img_url" id="img_url" value={fields.img_url} onChange={handleFieldsChange}/>
+        </div>
+        <div>
+          <label htmlFor="link">Link: </label>
+          <input type="text" name="link" id="link" value={fields.link} onChange={handleFieldsChange}/>
         </div>
         <br />
         <input type="submit" />
@@ -23,3 +50,12 @@ const Form = (props) => {
   )
 }
 export default Form
+
+
+// {
+//   "id": "uranus",
+//   "name": "Uranus",
+//   "description": "Uranus (from the Latin name Ūranus for the Greek god Οὐρανός) is the seventh planet from the Sun. It has the third-largest planetary radius and fourth-largest planetary mass in the Solar System.",
+//   "img_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Uranus2.jpg/280px-Uranus2.jpg",
+//   "link": "https://en.wikipedia.org/wiki/Uranus"
+// }
